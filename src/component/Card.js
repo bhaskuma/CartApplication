b6a5cff6bae6d44d0ab71f3c57e392ccc594d5e4
bhaskuma <bhaskarkumarrajput100@gmail.com>
2024-03-07 01:03:27 +0530

    add all the functionality of frontend

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Cardsdata from './CardsData'
+import "./style.css";
 
-const Card = () => {
+const Cards = () => {
+
+    const [data, setData] = useState(Cardsdata)
     return (
-        <div>Card</div>
+        <div className='container mt-3'>
+            <h2 className='text-center'>Add to Cart Projects</h2>
+
+            <div className="row d-flex justify-content-center align-items-center">
+                {
+                    data.map((element, id) => {
+                        return (
+                            <>
+                                <Card style={{ width: '22rem', border: "none" }} className="mx-2 mt-4 card_style">
+                                    <Card.Img variant="top" src={element.imgdata} style={{ height: "16rem" }} className="mt-3" />
+                                    <Card.Body>
+                                        <Card.Title>{element.rname}</Card.Title>
+                                        <Card.Text>
+                                            Price : ₹ {element.price}
+                                        </Card.Text>
+                                        <div className="button_div d-flex justify-content-center">
+                                            <Button variant="primary"
+                                                className='col-lg-12'>Add to Cart</Button>
+                                        </div>
+
+                                    </Card.Body>
+                                </Card>
+                            </>
+                        )
+                    })
+                }
+
+            </div>
+        </div>
     )
 }
 
-export default Card
+export default Cards
--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,23 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Badge from '@mui/material/Badge';
 import { NavLink } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
@@ -15,11 +30,34 @@ const Header = () => {
                         <NavLink to="/" className="text-decoration-none text-light">Home</NavLink>
 
                     </Nav>
-                    <Badge badgeContent={4} color="primary">
+                    <Badge badgeContent={4} color="primary"
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
                         <i class="fa-solid fa-cart-shopping text-white" style={{ fontSize: 25, cursor: "pointer" }}></i>
                     </Badge>
 
                 </Container>
+
+
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <div className='card_details d-flex justify-content-center' style={{ padding: "3px" }}>
+                        <i className='fas fa-close smallclose' onClick={handleClose} style={{ cursor: "pointer", position: "absolute", top: 2, right: 20 }}></i>
+                        <p>your cart is compaty</p>
+                    </div>
+
+                </Menu>
             </Navbar>
         </>
     )
